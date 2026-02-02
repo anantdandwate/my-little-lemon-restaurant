396db91c6c3882473d3a1685b5a04003e2bbb664
AD <anant.dandwate@capgemini.com>
2026-02-02 10:40:13 +0100

    Add Header, Nav, Main, and Footer components to App

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <img src="logo.png" alt="Little Lemon Logo" />
+            Header
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <nav>
+            <div className="navigation">
+                <a href="/">Home</a>
+                <a href="/about">Our Story</a>
+                <a href="/menu">Menu</a>
+                <a href="/booking">Book a Table</a>
+                <a href="/order">Order</a>
+                <a href="/signin">Sign In</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
